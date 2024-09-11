0494466aadbc0d135bdd43a729494c6f26e1dcdd
Andrei <andreirutty.ar20@outlook.com>
2024-09-11 08:59:31 +0400

    Replace stacks of <SkillsCard> component with skill dictionary and map function

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 import SkillsCard from "./SkillsCard";
-import html from "../public/assets/skills/html.png";
-import css from "../public/assets/skills/css.png";
-import tailwind from "../public/assets/skills/tailwind.png";
-import javascript from "../public/assets/skills/javascript.png";
-import react from "../public/assets/skills/react.png";
-import node from "../public/assets/skills/node.png";
-import express from "../public/assets/skills/express.png";
-import mongoDB from "../public/assets/skills/mongodb.png";
-import github from "../public/assets/skills/github.png";
-import mysql from "../public/assets/skills/mysql.png";
-import nextjs from "../public/assets/skills/nextjs.png";
-import python from "../public/assets/skills/python.png";
-import c from "../public/assets/skills/c.png";
-import cplusplus from "../public/assets/skills/cplusplus.png";
-import shell from "../public/assets/skills/shell.png";
+import skillsData from "../pages/api/skills";
 
 const Skills = () => {
+  const skills = skillsData;
+
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
@@ -25,21 +13,9 @@ const Skills = () => {
         </p>
         <h2 className="py-4 text-center md:text-left">What Tools I Use</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <SkillsCard skill="html" icon={html} />
-          <SkillsCard skill="css" icon={css} />
-          <SkillsCard skill="tailwind" icon={tailwind} />
-          <SkillsCard skill="javascript" icon={javascript} />
-          <SkillsCard skill="react" icon={react} />
-          <SkillsCard skill="node" icon={node} />
-          <SkillsCard skill="express" icon={express} />
-          <SkillsCard skill="mongodb" icon={mongoDB} />
-          <SkillsCard skill="github" icon={github} />
-          <SkillsCard skill="mysql" icon={mysql} />
-          <SkillsCard skill="nextjs" icon={nextjs} />
-          <SkillsCard skill="python" icon={python} />
-          <SkillsCard skill="C" icon={c} />
-          <SkillsCard skill="C++" icon={cplusplus} />
-          <SkillsCard skill="shell" icon={shell} />
+          {skills.map(({ skill, icon }) => (
+            <SkillsCard key={skill} skill={skill} icon={icon} />
+          ))}
         </div>
       </div>
     </div>
---
+++ pages/api/skills.js
@@ -0,0 +1,35 @@
+import html from "../../public/assets/skills/html.png";
+import css from "../../public/assets/skills/css.png";
+import tailwind from "../../public/assets/skills/tailwind.png";
+import javascript from "../../public/assets/skills/javascript.png";
+import react from "../../public/assets/skills/react.png";
+import node from "../../public/assets/skills/node.png";
+import express from "../../public/assets/skills/express.png";
+import mongoDB from "../../public/assets/skills/mongodb.png";
+import github from "../../public/assets/skills/github.png";
+import mysql from "../../public/assets/skills/mysql.png";
+import nextjs from "../../public/assets/skills/nextjs.png";
+import python from "../../public/assets/skills/python.png";
+import c from "../../public/assets/skills/c.png";
+import cplusplus from "../../public/assets/skills/cplusplus.png";
+import shell from "../../public/assets/skills/shell.png";
+
+const skillsData = [
+    { skill: "html", icon: html },
+    { skill: "css", icon: css },
+    { skill: "tailwind", icon: tailwind },
+    { skill: "javascript", icon: javascript },
+    { skill: "react", icon: react },
+    { skill: "node", icon: node },
+    { skill: "express", icon: express },
+    { skill: "mongodb", icon: mongoDB },
+    { skill: "github", icon: github },
+    { skill: "mysql", icon: mysql },
+    { skill: "nextjs", icon: nextjs },
+    { skill: "python", icon: python },
+    { skill: "C", icon: c },
+    { skill: "C++", icon: cplusplus },
+    { skill: "shell", icon: shell },
+]
+
+export default skillsData
