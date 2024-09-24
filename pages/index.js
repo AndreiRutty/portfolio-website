df9844c7c28a43814f17964f7ba045a9581120a6
Andrei <andreirutty.ar20@outlook.com>
2024-09-24 12:28:29 +0400

    Removed Contact Section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Main from "../components/Main";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
-import Contact from "../components/Contact";
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
-      <Contact />
+      <Footer />
     </div>
   );
 }
